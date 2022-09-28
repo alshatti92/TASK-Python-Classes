27b698718755a94ebab1c2ed5570969bc4f71d32
alshatti92 <a.alshatti@hotmail.co.uk>
2022-09-28 20:33:22 +0300

    not_completed

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 class Wallet:
     def __init__(self, money):
-        pass
+        self.money = money
+       
 
-    def credit(amount):
-        pass
 
-    def debit(amount):
-        pass
+    def credit(self, amount):
+        self.money = amount + self.money
+
+    def debit(self, amount):
+        self.money = self.money - amount
 
 
 wallet = Wallet(6)
-wallet = Wallet()  # This should default money inside the wallet to 0
-print(wallet)
+wallet = Wallet(0)  # This should default money inside the wallet to 0
+print(Wallet)
 
-
+wallet ():
+amount = 5
+credit = 
 class Person:
+    def __init__(self, name, location, wallet, moveto):
+        self.name = name
+        self.location = location
+        self.wallet = wallet
+        self.moveto = moveto
+        pass
     # Implement the code here
     pass
 
