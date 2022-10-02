024d9d39d03fd51a199e1af70097301f9c8ec8dd
alshatti92 <a.alshatti@hotmail.co.uk>
2022-10-02 18:28:48 +0300

    updated and needs more correction

--- main.py
+++ main.py
@@ -1,48 +1,71 @@
 class Wallet:
-    def __init__(self, money):
+    def __init__(self, money=0):
         self.money = money
-       
-
+    
 
     def credit(self, amount):
         self.money = amount + self.money
 
+        # we can print for testing perpose ... print("f credit: the new amount of money is {self.money}")
+
     def debit(self, amount):
         self.money = self.money - amount
 
+    def __str__(self):
+        return f"this wallet has {self.money}"
+        # we use this to test print.
+
 
 wallet = Wallet(6)
-wallet = Wallet(0)  # This should default money inside the wallet to 0
+wallet = Wallet()  # This should default money inside the wallet to 0
 print(Wallet)
 
-wallet ():
-amount = 5
-credit = 
+
 class Person:
-    def __init__(self, name, location, wallet, moveto):
+    def __init__(self, name, location, money):
         self.name = name
         self.location = location
-        self.wallet = wallet
-        self.moveto = moveto
-        pass
-    # Implement the code here
-    pass
+        self.wallet = wallet (money)
+        
+    def __str__(self):
+        return f"this person is {self.name} is {self.location}"
+
+    def move_to(self, point):
+        self.location = point
+        print(f"the new location for {self.name} is {self.location}")
+    
 
 
 person = Person("Moh", 5, 50)
 
 
-class Vendor:
-    # implement Vendor!
-    pass
+class Vendor(Person):
+    range = 5
+    price = 1
+
+    def __init__(self, name, location, money):
+        super().__init__(name, location. money)
+
+
 
 
 vendor = Vendor("Abdallah", 3, 6)
 
 
-class Customer:
-    # implement Customer!
-    pass
+class Customer(Person):
+   def __init__(self, name, location, money):
+       super().__init__(name, location, money)
 
+    
+    def is_in_range(self, vendor):
+        distance = abs(self.location - vendor.location)
+        if distance <= vendor.range:
+            print(f"this vendor {vendor.name} is within {self.name} range")
+            return True
+        else:
+            print(f"this vendor {vendor.name} is NOT within {slef.name} range")
+            return False
 
+vendor_one = Vendor("talal", 3, 60)
+vendor_two = Vendor ("lateefa", 81, 500)
 customer = Customer("Abdallah", 3, 6)
